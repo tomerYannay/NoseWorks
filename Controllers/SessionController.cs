0cbe13866a1b3696c907fffe0b0c873c24d2f6c4
Tomer Yannay <tomeryannay2602@gmail.com>
2025-04-01 16:08:36 +0300

    fix session

--- Controllers/SessionController.cs
+++ Controllers/SessionController.cs
@@ -17,9 +17,9 @@ namespace MyFirstMvcApp.Controllers
     public class SessionController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public SessionController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        public SessionController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _userManager = userManager;
